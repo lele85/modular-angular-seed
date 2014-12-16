833e1a8adc80e6097bd5f0fc326e4140c66c83b4
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2014-12-16 15:16:15 +0100

    Passed stub tests to mocha

--- scripts/test/modular_spec.js
+++ scripts/test/modular_spec.js
@@ -1,17 +1,38 @@
-jasmine.getEnv().defaultTimeoutInterval = 50000;
-describe("Modular seed", function() {
-	var cwd = __dirname + "/../";
+var cwd = __dirname + "/../";
+var expect = require("chai").expect,
+	spawn = require("child_process").spawn,
+	selenium;
 
-	["sass", "build", "test_unit", "selenium_update"].forEach(function(task) {
+describe("Modular seed", function() {
+	before(function(done) {
+		selenium = spawn("grunt", ["selenium"], {
+			cwd: cwd
+		});
+		setTimeout(done, 4000);
+	});
+
+	[
+		"selenium_update",
+		"sass",
+		"build",
+		"test_unit",
+		"selenium_update",
+		"test_e2e"
+	].forEach(function(task) {
 		it("should run grun task " + task, function(done) {
-			var spawn = require("child_process").spawn;
+
 			var act = spawn("grunt", [task], {
 				cwd: cwd
 			});
 			act.on("close", function(code) {
-				expect(code).toBe(0);
+				expect(code).to.equal(0);
 				done();
 			});
 		});
 	});
+
+	after(function() {
+		selenium.kill();
+	});
+
 });
