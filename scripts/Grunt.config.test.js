120d2ebb6083d038266ea56cc0f836799c28ef38
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2014-12-17 01:19:09 +0100

    If e2e test fail build fails too

--- scripts/Grunt.config.test.js
+++ scripts/Grunt.config.test.js
@@ -24,7 +24,7 @@ module.exports = {
 	},
 	protractor: {
 		options: {
-			keepAlive: true, // If false, the grunt process stops when the test fails.
+			keepAlive: false, // If false, the grunt process stops when the test fails.
 			noColor: false, // If true, protractor will not use colors in its output.
 			configFile: "./protractor.config.js"
 		},
