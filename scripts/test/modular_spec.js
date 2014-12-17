13cabc8e0aff86fb63a72d24231e642fa497569f
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2014-12-17 01:01:01 +0100

    Stdio inherited in mocha tests and first e2e test

--- scripts/test/modular_spec.js
+++ scripts/test/modular_spec.js
@@ -6,7 +6,8 @@ var expect = require("chai").expect,
 describe("Modular seed", function() {
 	before(function(done) {
 		selenium = spawn("grunt", ["selenium"], {
-			cwd: cwd
+			cwd: cwd,
+			stdio: "inherit"
 		});
 		setTimeout(done, 4000);
 	});
@@ -16,19 +17,38 @@ describe("Modular seed", function() {
 		"sass",
 		"build",
 		"test_unit",
-		"selenium_update",
-		"test_e2e"
+		"selenium_update"
 	].forEach(function(task) {
 		it("should run grun task " + task, function(done) {
 
 			var act = spawn("grunt", [task], {
-				cwd: cwd
+				cwd: cwd,
+				stdio: "inherit"
 			});
 			act.on("close", function(code) {
 				expect(code).to.equal(0);
 				done();
 			});
 		});
+	});
+
+	it("Should serve app with customer specific conf", function(done) {
+		var serve = spawn("grunt", ["serve"], {
+			cwd: cwd,
+			stdio: "inherit"
+		});
+		setTimeout(function() {
+			var test = spawn("grunt", ["test_e2e"], {
+				cwd: cwd,
+				stdio: "inherit"
+			});
+			test.on("close", function(code) {
+				expect(code).to.equal(0);
+				serve.kill();
+
+				done();
+			});
+		}, 4000);
 	});
 
 	after(function() {
